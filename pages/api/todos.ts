177f33e27219ba99e74c4de74687028d24d13978
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-11-27 17:32:44 +0300

    add an api route for fetching todos

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Todo } from '../../types';
 
 type Data = {
-  name: string
+  todos: Todo[];
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ todos: [{ id: 1, content: 'Learn Next.js' }] });
 }
