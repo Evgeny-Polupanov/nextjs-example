f350f5ec3cd34082cfc05ffbdabd68741246e7e9
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-11-27 21:49:06 +0300

    Add removal and marking of hardcoded todos

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -5,9 +5,37 @@ type Data = {
   todos: Todo[];
 }
 
+const todos = [
+  { id: 1, content: 'Learn Next.js', isDone: false },
+  { id: 2, content: 'Learn Node.js', isDone: false },
+];
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ todos: [{ id: 1, content: 'Learn Next.js' }] });
+  let id: string | number;
+  switch (req.method) {
+    case 'GET':
+      res.status(200).json({ todos });
+      break;
+    case 'DELETE':
+      id = JSON.parse(req.body).id;
+      if (id) {
+        const todoIndexToDelete = todos.findIndex((todo) => String(todo.id) === String(id));
+        todos.splice(todoIndexToDelete, 1);
+        res.status(200).json({ todos });
+      }
+      break;
+    case 'PATCH':
+      id = JSON.parse(req.body).id;
+      if (id) {
+        const todoIndexToSetIsDone = todos.findIndex((todo) => String(todo.id) === String(id));
+        todos[todoIndexToSetIsDone].isDone = !todos[todoIndexToSetIsDone].isDone;
+        res.status(200).json({ todos });
+      }
+      break;
+    default:
+      throw Error('Method is not supported.');
+  }
 }
