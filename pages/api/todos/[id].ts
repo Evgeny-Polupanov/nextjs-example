52cf8da42ac17eb3b3271dc81ed16dfa2202216c
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-11-28 12:54:09 +0300

    make api routes with IDs

---
+++ pages/api/todos/[id].ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Todo } from '../../../types';
+import { todos } from '../todos';
+
+type Data = {
+  todos: Todo[];
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { id } = req.query;
+  switch (req.method?.toUpperCase()) {
+    case 'DELETE':
+      const todoIndexToDelete = todos.findIndex((todo) => String(todo.id) === String(id));
+      if (todoIndexToDelete !== -1) {
+        todos.splice(todoIndexToDelete, 1);
+        res.status(200).json({ todos });
+      } else {
+        res.status(404).json({ todos });
+      }
+      break;
+    case 'PATCH':
+      const todoIndexToSetIsDone = todos.findIndex((todo) => String(todo.id) === String(id));
+      if (todoIndexToSetIsDone !== -1) {
+        todos[todoIndexToSetIsDone].isDone = !todos[todoIndexToSetIsDone].isDone;
+        res.status(200).json({ todos });
+      } else {
+        res.status(404).json({ todos });
+      }
+      break;
+    default:
+      res.status(405).json({ todos });
+      throw Error('Method is not supported.');
+  }
+}
